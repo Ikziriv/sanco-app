cc4b3248d65981133303b292b68d39dd5886cf24
Rizky Ivan Darmawan <ivanriski007@gmail.com>
2021-10-27 14:52:52 +0700

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 
 const config = {
 	purge: {
-		mode: 'jit',
+		// mode: 'jit',
 		content: ['./src/**/*.{html,js,svelte,ts}'],
 		options: {
 			defaultExtractor: (content) => [
